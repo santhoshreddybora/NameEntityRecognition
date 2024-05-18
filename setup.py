9a04e500e5cccbb20873c834d9923046fd0ec512
santhoshreddybora <borasanthosh921@gmai.com>
2024-05-18 14:32:02 +0530

    Add template, log,excpetion,utils files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages,setup
+
+setup(name='ner-using-bert',
+      version='0.0.1',
+      author='Santhosh',
+      author_email='borsanthosh921@gmail.com',
+      packages=find_packages(),
+      install_requires=[])
